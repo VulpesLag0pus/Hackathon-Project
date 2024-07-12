98494f72284335ea758d64ebd99a9e3d79ba0d67
VulpesLag0pus <Tiesvandennieuwenhof@gmail.com>
2024-07-12 10:08:12 +0200

    Added footer and SideNav

---
+++ Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaHome, FaClipboardList, FaCalendarAlt, FaGamepad } from 'react-icons/fa';
+import logo from '../app/sipnplay.png';
+import '../app/Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div className="d-flex flex-column vh-100 p-3 bg-light" style={{ width: '250px' }}>
+            <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <img src={logo} alt="Logo" className="bi me-2" width="40" height="32" />
+                <span className="fs-4">sip & play</span>
+            </a>
+            <hr />
+            <ul className="nav nav-pills flex-column mb-auto">
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-dark">
+                        <FaHome className="me-2" />
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="/order" className="nav-link text-dark">
+                        <FaClipboardList className="me-2" />
+                        Order
+                    </a>
+                </li>
+                <li>
+                    <a href="/events" className="nav-link text-dark">
+                        <FaCalendarAlt className="me-2" />
+                        Events
+                    </a>
+                </li>
+                <li>
+                    <a href="/games" className="nav-link text-dark">
+                        <FaGamepad className="me-2" />
+                        Games
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;

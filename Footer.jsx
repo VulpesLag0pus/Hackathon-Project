b4749acb18c4e1d48240f362c8d0bac91a36755c
VulpesLag0pus <Tiesvandennieuwenhof@gmail.com>
2024-07-12 09:35:29 +0200

    Added footer

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Col, Row} from "react-bootstrap";
+
+const Footer = () => {
+    return (
+        <>
+            <Row className={"mt-5"}>
+                <div className="footer text-center">
+                    <span>©2020 by Sip & Play</span>
+                </div>
+            </Row>
+        </>
+    );
+};
+
+export default Footer;

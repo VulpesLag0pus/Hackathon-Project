cbf39492dae7a055e158c1983120d71dd2881590
VulpesLag0pus <Tiesvandennieuwenhof@gmail.com>
2024-07-12 14:15:31 +0200

    Added 3D files and made an array of object to display the models

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  MenuItems = [{path: './Coffee.gbl', name: 'Latte', price: '4,95'}, {path: './IceCoffee.gbl', name: 'Ice Coffee', price: '5,95'}, {path: './IceCoffee.gbl', name: 'Ice Coffee', price: '5,95'}]
+  MenuItems = [{path: './Coffee.gbl', name: 'Coffee', price: '4,95'}, {path: './IceCoffee.gbl', name: 'Ice Coffee', price: '5,95'}, {path: './CafeLatte.gbl', name: 'Latte', price: '5,50'}, {path: './BobaTea.gbl', name: 'Boba Tea', price: '6,50'}, {path: './BubbleTeaCookies.glb', name: 'Bubble Tea + Cookie', price: '6,95'}, {path: './BobaTea.gbl', name: 'Boba Tea', price: '6,50'}, {path: './Desserts.glb', name: 'Desserts', price: '7,95'}]
   return (<div>    
     <Sidebar/>
     <section></section>
